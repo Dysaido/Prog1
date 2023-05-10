974ad3ff199017d55f4b486a1979d80a8732dca9
Dysaido <tonyyoni@gmail.com>
2023-05-10 20:52:33 +0200

    optimalizalas

--- 10/lotto.c
+++ 10/lotto.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 #define OSSZEG 90
 #define SZORZAT 996300
+#define MERET 45
+#define ARRAY_SIZE 6
 
-void keres(int index, int intervalsize, int interval[], int tipp[], int aktualis_osz, int aktualis_szor)
+void keres(int count, int index, int numbers[], int sum, int multiply)
 {
-    if (index == 6)
+    if (count == 6)
     {
-        if ((SZORZAT == aktualis_szor) && (OSSZEG == aktualis_osz))
+        if ((SZORZAT == multiply) && (OSSZEG == sum))
         {
             printf("6db szam: ");
-            for (int i = 0; i < index; ++i)
+            for (int i = 0; i < ARRAY_SIZE; ++i)
             {
-                printf("%d ", tipp[i]);
+                printf("%d ", numbers[i]);
             }
             puts("");
-            exit(0);
         }
         return;
     }
-    for (int i = 0; i < intervalsize; ++i)
+    
+    for (int i = index + 1; i <= MERET - (ARRAY_SIZE - count); ++i)
     {
-        tipp[index] = interval[i];
-        keres(index + 1, intervalsize, interval, tipp, aktualis_osz + interval[i], aktualis_szor * interval[i]);
+        numbers[count] = i;
+        keres(count + 1, i, numbers, sum + i, multiply * i);
     }
 }
 
 int main()
 {
-    int interval[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45};
-    int interalsize = sizeof(interval) / sizeof(interval[0]);
-    int tipp[6];
-    keres(0, interalsize, interval, tipp, 0, 1);
-    /*for (int i = 1; i < 46; ++i)
-    {
-        printf("%d, ", i);
-    }
-    puts("");*/
+    int numbers[6];
+    keres(0, 0, numbers, 0, 1);
+
     return 0;
 }
